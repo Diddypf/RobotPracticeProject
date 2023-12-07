e1cc0594631ce6cc573a2773ff9f60b55812ea73
Team 1038 <team1038@1038.org>
2023-12-06 20:02:30 -0500

    lihfviupwfheog

--- robotpractice project/src/main/java/frc/robot/constants/CubeShooterConstants.java
+++ robotpractice project/src/main/java/frc/robot/constants/CubeShooterConstants.java
@@ -3,5 +3,5 @@ package frc.robot.constants;
 public class CubeShooterConstants {
     public static final int kCubeFeedermotorport = 0;
     public static final int kcubeShootermotorport = 0;
-
+    
 }
--- robotpractice project/src/main/java/frc/robot/subsystems/CubeShooter.java
+++ robotpractice project/src/main/java/frc/robot/subsystems/CubeShooter.java
@@ -7,8 +7,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CubeShooterConstants;
 
 public final class CubeShooter extends SubsystemBase{
-    
+    private static CubeShooter instance;
+
     private CANSparkMax FeederMotor = new CANSparkMax(CubeShooterConstants.kCubeFeedermotorport,MotorType.kBrushless);
-    
     private CANSparkMax ShoterMotor = new CANSparkMax(CubeShooterConstants.kcubeShootermotorport,MotorType.kBrushless);
+
+    public static CubeShooter getinstance() {
+        
+        if(instance == null){
+            instance = new CubeShooter();
+        }
+        return instance;
+    }
+    private CubeShooter(){
+        FeederMotor.restoreFactoryDefaults();
+        FeederMotor.burnFlash();
+        ShoterMotor.restoreFactoryDefaults();  
+        ShoterMotor.burnFlash();
+
+    }
 }
