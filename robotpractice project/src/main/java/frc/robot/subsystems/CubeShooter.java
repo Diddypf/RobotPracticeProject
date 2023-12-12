0ad144a6ac416a6121017c87e66d64583f502d58
Team 1038 <team1038@1038.org>
2023-12-11 19:22:26 -0500

    fixed some conventions from feedback

--- robotpractice project/src/main/java/frc/robot/subsystems/CubeShooter.java
+++ robotpractice project/src/main/java/frc/robot/subsystems/CubeShooter.java
@@ -9,10 +9,10 @@ import frc.robot.constants.CubeShooterConstants;
 public final class CubeShooter extends SubsystemBase{
     private static CubeShooter instance;
 
-    private CANSparkMax FeederMotor = new CANSparkMax(CubeShooterConstants.kCubeFeedermotorport,MotorType.kBrushless);
-    private CANSparkMax ShoterMotor = new CANSparkMax(CubeShooterConstants.kcubeShootermotorport,MotorType.kBrushless);
+    private CANSparkMax feederMotor = new CANSparkMax(CubeShooterConstants.kCubeFeedermotorport,MotorType.kBrushless);
+    private CANSparkMax shooterMotor = new CANSparkMax(CubeShooterConstants.kcubeShootermotorport,MotorType.kBrushless);
 
-    public static CubeShooter getinstance() {
+    public static CubeShooter getInstance() {
         
         if(instance == null){
             instance = new CubeShooter();
@@ -20,10 +20,10 @@ public final class CubeShooter extends SubsystemBase{
         return instance;
     }
     private CubeShooter(){
-        FeederMotor.restoreFactoryDefaults();
-        FeederMotor.burnFlash();
-        ShoterMotor.restoreFactoryDefaults();  
-        ShoterMotor.burnFlash();
+        feederMotor.restoreFactoryDefaults();
+        shooterMotor.restoreFactoryDefaults();  
+        feederMotor.burnFlash();
+        shooterMotor.burnFlash();
 
     }
 }
